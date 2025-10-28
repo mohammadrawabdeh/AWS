1df64fcdd09310b68953163810be7d349e4c4f8f
mohammadrawabdeh <mohammad.rawabdeh@cloudvests.com>
2025-10-28 15:25:38 +0300

    Delete biniding.tf

--- biniding.tf
+++
@@ -1,9 +0,0 @@
-resource "google_project_iam_binding" "aws_readonly_binding" {
-  project = "my-project-mohammad-476307"
-  role    = "projects/my-project-mohammad-476307/roles/readonlyAccessRole"
-
-  members = [
-    "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.aws_pool.name}/attribute.aws_role/arn:aws:sts::926837946404:assumed-role/AWStoGCP"
-  ]
-}
-
